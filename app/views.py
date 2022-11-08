3cc489d7e7658388324df6364f64fe79336e275b
alekso <sasha.oparin.1984@mail.ru>
2022-11-08 14:32:39 +0300

    Add tag_list template

--- app/views.py
+++ app/views.py
@@ -16,8 +16,17 @@ class Index:
 class Question:
     def question(request, question_id: int):
         question_item = models.QUESTIONS[question_id]
-        context = {'question': question_item}
+        context = {
+            'question': question_item,
+            'popular_tags': models.TAGS,
+            'best_users': models.BEST_USERS,
+        }
         return render(request, 'question.html', context=context)
+
+class Ask:
+    def ask(request):
+        context = {}
+        return render(request, 'ask.html', context=context)
 
 # def ask(request):
 #     return render(request, 'ask.html')
