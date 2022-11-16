c1be5ff6e3e75b5815b5694497c50eadb1afcd8d
Aleksandr <sasha.oparin.1984@mail.ru>
2022-11-16 15:20:39 +0300

    Add models

--- app/models.py
+++ app/models.py
@@ -1,37 +1,99 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import User
 
-import random # for random data
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
+from django.db.models import Sum, Count
 
-USER = {
-        'login': 'aleks',
-        'email': 'aleks@mail.ru',
-        'nick_name': 'aleks666',
-        'avatar': '',
-        'is_auth': False,
-    }
+from django.db.models.deletion import CASCADE
 
-QUESTIONS = [
-    {
-        'id': question_id,
-        'like_count': random.randint(1, 100),
-        'title': f'Quesion {question_id}',
-        'text': f'Text of question {question_id}',
-        'answers_count': random.randint(1, 10),
-        'tags': [f'tag {i}' for i in range(random.randint(1, 10))],
-        'answers': [],
-    } for question_id in range(10)
-]
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    tags = models.ManyToManyField('Tag', related_name='questions')
+    author = models.ForeignKey('Profile', models.CASCADE, related_name='questions')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    votes = GenericRelation('Like', related_query_name='questions')
 
-for question in QUESTIONS:
-    question['answers'] = [
-        {
-            'id': answer_id,
-            'like_count': random.randint(1, 100),
-            'text': f'Text of answer {answer_id}',
-            'correct': random.choice([True, False]),
-        } for answer_id in range(question['answers_count'])
-    ]
+    # def __str__(self):
+    #     return self.title
 
-TAGS = [f'Popular tag {tag_id}' for tag_id in range(random.randint(5, 10))]
+class Answer(models.Model):
+    text = models.TextField()
+    question = models.ForeignKey(Question, models.CASCADE, related_name='answers')
+    correct = models.BooleanField(default=False)
+    author = models.ForeignKey('Profile', models.CASCADE, related_name='answers')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    votes = GenericRelation('Like', related_name='answers')
 
-BEST_USERS = [f'User {user_id}' for user_id in range(random.randint(2, 7))]
+    # def __str__(self):
+    #     return (self.text[:40] + '...')
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    # def __str__(self):
+    #     return self.name
+
+class Profile(models.Model):
+    avatar = models.ImageField(blank = True, upload_to = "DBImages/")
+    user = models.OneToOneField(User, models.CASCADE)
+
+    # def __str__(self):
+    #     return self.user.username
+
+class Like(models.Model):
+    LIKE = 1
+    NOTHING = 0
+    DISLIKE = -1
+ 
+    VOTES = (
+        (LIKE, 'Like'),
+        (NOTHING, 'Nothing'),
+        (DISLIKE, 'Dislike')
+    )
+ 
+    vote = models.SmallIntegerField(choices=VOTES, default=NOTHING)
+    user = models.ForeignKey('Profile', models.CASCADE, related_name='likes')
+ 
+    content_type = models.ForeignKey(ContentType, models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
+
+
+# import random # for random data
+
+# USER = {
+#         'login': 'aleks',
+#         'email': 'aleks@mail.ru',
+#         'nick_name': 'aleks666',
+#         'avatar': '',
+#         'is_auth': False,
+#     }
+
+# QUESTIONS = [
+#     {
+#         'id': question_id,
+#         'like_count': random.randint(1, 100),
+#         'title': f'Quesion {question_id}',
+#         'text': f'Text of question {question_id}',
+#         'answers_count': random.randint(1, 10),
+#         'tags': [f'tag {i}' for i in range(random.randint(1, 10))],
+#         'answers': [],
+#     } for question_id in range(10)
+# ]
+
+# for question in QUESTIONS:
+#     question['answers'] = [
+#         {
+#             'id': answer_id,
+#             'like_count': random.randint(1, 100),
+#             'text': f'Text of answer {answer_id}',
+#             'correct': random.choice([True, False]),
+#         } for answer_id in range(question['answers_count'])
+#     ]
+
+# TAGS = [f'Popular tag {tag_id}' for tag_id in range(random.randint(5, 10))]
+
+# BEST_USERS = [f'User {user_id}' for user_id in range(random.randint(2, 7))]
--- app/views.py
+++ app/views.py
@@ -2,104 +2,104 @@ from django.http import HttpResponse
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
-from . import models
+# from . import models
 
 
 # class Index:
-def index(request):
-    page_questions = paginate(models.QUESTIONS, request, 4)
+# def index(request):
+#     page_questions = paginate(models.QUESTIONS, request, 4)
     
-    context = {
-        'user_info': models.USER,
-        'questions': page_questions,
-        'popular_tags': models.TAGS,
-        'best_users': models.BEST_USERS,
-        }
-    return render(request, 'index.html', context=context)
+#     context = {
+#         'user_info': models.USER,
+#         'questions': page_questions,
+#         'popular_tags': models.TAGS,
+#         'best_users': models.BEST_USERS,
+#         }
+#     return render(request, 'index.html', context=context)
 
-# class Question:
-def question(request, question_id: int):
-    question_item = None
-    if any(question['id'] == question_id for question in models.QUESTIONS):
-        question_item = models.QUESTIONS[question_id]
-    else:
-        return render(request, 'errors/404.html')
+# # class Question:
+# def question(request, question_id: int):
+#     question_item = None
+#     if any(question['id'] == question_id for question in models.QUESTIONS):
+#         question_item = models.QUESTIONS[question_id]
+#     else:
+#         return render(request, 'errors/404.html')
 
-    question_page_answers = paginate(question_item['answers'], request, 4)
+#     question_page_answers = paginate(question_item['answers'], request, 4)
 
-    context = {
-        'user_info': models.USER,
-        'question': question_item,
-        'answers': question_page_answers,
-        'popular_tags': models.TAGS,
-        'best_users': models.BEST_USERS,
-    }
-    return render(request, 'question.html', context=context)
+#     context = {
+#         'user_info': models.USER,
+#         'question': question_item,
+#         'answers': question_page_answers,
+#         'popular_tags': models.TAGS,
+#         'best_users': models.BEST_USERS,
+#     }
+#     return render(request, 'question.html', context=context)
 
-# class Ask:
-def ask(request):
-    context = {
-        'user_info': models.USER,
-        'popular_tags': models.TAGS,
-        'best_users': models.BEST_USERS
-    }
-    return render(request, 'ask.html', context=context)
+# # class Ask:
+# def ask(request):
+#     context = {
+#         'user_info': models.USER,
+#         'popular_tags': models.TAGS,
+#         'best_users': models.BEST_USERS
+#     }
+#     return render(request, 'ask.html', context=context)
 
-# class Tag:
-def tag(request, tag_name: str):
-    questions_with_tag = list(filter(lambda question: tag_name in question['tags'], models.QUESTIONS))
-    if not any(questions_with_tag):
-        return render(request, 'errors/404.html')
+# # class Tag:
+# def tag(request, tag_name: str):
+#     questions_with_tag = list(filter(lambda question: tag_name in question['tags'], models.QUESTIONS))
+#     if not any(questions_with_tag):
+#         return render(request, 'errors/404.html')
 
-    page_questions_with_tag = paginate(questions_with_tag, request, 4)
+#     page_questions_with_tag = paginate(questions_with_tag, request, 4)
 
-    context = {
-        'user_info': models.USER,
-        'tag_name': tag_name,
-        'questions': page_questions_with_tag,
-        'popular_tags': models.TAGS,
-        'best_users': models.BEST_USERS
-    }
-    return render(request, 'tag.html', context=context)
+#     context = {
+#         'user_info': models.USER,
+#         'tag_name': tag_name,
+#         'questions': page_questions_with_tag,
+#         'popular_tags': models.TAGS,
+#         'best_users': models.BEST_USERS
+#     }
+#     return render(request, 'tag.html', context=context)
 
-# class Login:
-def login(request):
-    context = {
-        'popular_tags': models.TAGS,
-        'best_users': models.BEST_USERS
-    }
-    return render(request, 'login.html', context=context)
+# # class Login:
+# def login(request):
+#     context = {
+#         'popular_tags': models.TAGS,
+#         'best_users': models.BEST_USERS
+#     }
+#     return render(request, 'login.html', context=context)
 
-# class Settings:
-def settings(request):
-    context = {
-        'user_info': models.USER,
-        'popular_tags': models.TAGS,
-        'best_users': models.BEST_USERS
-    }
-    return render(request, 'settings.html', context=context)
+# # class Settings:
+# def settings(request):
+#     context = {
+#         'user_info': models.USER,
+#         'popular_tags': models.TAGS,
+#         'best_users': models.BEST_USERS
+#     }
+#     return render(request, 'settings.html', context=context)
 
-# class Signup:
-def signup(request):
-    context = {
-        'popular_tags': models.TAGS,
-        'best_users': models.BEST_USERS
-    }
-    return render(request, 'signup.html', context=context)
+# # class Signup:
+# def signup(request):
+#     context = {
+#         'popular_tags': models.TAGS,
+#         'best_users': models.BEST_USERS
+#     }
+#     return render(request, 'signup.html', context=context)
 
-# class HotQuestions:
-def hot_questions(request):
-    hot_questions = list(filter(lambda question: question['like_count'] >= 50, models.QUESTIONS))
-    if not any(hot_questions):
-        return render(request, 'errors/404.html')
+# # class HotQuestions:
+# def hot_questions(request):
+#     hot_questions = list(filter(lambda question: question['like_count'] >= 50, models.QUESTIONS))
+#     if not any(hot_questions):
+#         return render(request, 'errors/404.html')
 
-    context = {
-        'user_info': models.USER,
-        'questions': hot_questions,
-        'popular_tags': models.TAGS,
-        'best_users': models.BEST_USERS
-    }
-    return render(request, 'hot_questions.html', context=context)
+#     context = {
+#         'user_info': models.USER,
+#         'questions': hot_questions,
+#         'popular_tags': models.TAGS,
+#         'best_users': models.BEST_USERS
+#     }
+#     return render(request, 'hot_questions.html', context=context)
 
 def paginate(objects_list, request, items_per_page=10):
     paginator = Paginator(objects_list, items_per_page)
