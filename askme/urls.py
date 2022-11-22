85f6b8eaa3829dc88b293731472e2883c6d854b7
Aleksandr <sasha.oparin.1984@mail.ru>
2022-11-22 15:36:08 +0300

    Change urls location

--- askme/urls.py
+++ askme/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app.askme')),
+    path('', include('app.urls')),
 ]
